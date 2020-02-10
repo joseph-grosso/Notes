bf8673b5bf520e6a401fe6e7b014c18005dd0657
joegr <joe.grosso@cogeco.ca>
2020-02-10 11:03:06 -0500

    Update location of switch menu

--- switch_menu.sh
+++ switch_menu.sh
@@ -7,7 +7,7 @@ for file in $CURRENT_SEM
 do 
 	ARRAY+=
 done	
-selected=$(ls -d ~/Notes/bachelor-3/semester-2/*/ | cut -c41-100 | rofi -dmenu -p "Select your current class:" -theme nord -i -no-fixed-num-lines -location 2)
+selected=$(ls -d ~/Notes/bachelor-3/semester-2/*/ | cut -c41-100 | rofi -dmenu -p "Select your current class:" -theme nord -i -no-fixed-num-lines)
 
 if [ -z "$selected" ]
 then 
