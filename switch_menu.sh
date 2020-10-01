40d6135441c0af119c51115ed2c0a1c72ba96cd9
joegr <joe.grosso@cogeco.ca>
2020-10-01 00:55:01 -0400

    Fix variable naming conventions on switch_menu script

--- switch_menu.sh
+++ switch_menu.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
-CURRENT_SEM="~/Notes/bachelor-3/semester-2/*"
+CURRENT_SEM="/home/joegr/Notes/bachelor-4/semester-1/"
 
 ARRAY='' 
-for file in $CURRENT_SEM 
+for file in "${CURRENT_SEM}*" 
 do 
 	ARRAY+=
 done	
-selected=$(ls -d ~/Notes/bachelor-3/semester-2/*/ | cut -c41-100 | rofi -dmenu -p "Select your current class:" -theme nord -i -no-fixed-num-lines)
+selected=$(ls -d ~/Notes/bachelor-4/semester-1/*/ | cut -c41-100 | rofi -dmenu -p "Select your current class:" -theme nord -i -no-fixed-num-lines)
 
 if [ -z "$selected" ]
 then 
@@ -15,5 +15,4 @@ then
 fi 
 rm ~/Notes/current-course
 
-ln -s "/home/joegr/Notes/bachelor-3/semester-2/${selected}" ~/Notes/current-course 
-echo "Your current course is $selected"
+ln -s "${CURRENT_SEM}${selected}" ~/Notes/current-course 
